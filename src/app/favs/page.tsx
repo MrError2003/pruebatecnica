410a607ba2aa9605349e40980c68a5db87b38749
JuanZapata <jdzapatamar@cesde.net>
2024-11-27 23:05:48 -0500

    Otros cmabios

--- src/app/favs/page.tsx
+++ src/app/favs/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import React, { useState, useEffect } from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Background from "../components/background";
